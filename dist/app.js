7a4f0fe966fbcd81a143f76f37af84da7964bee5
cmjiwa <wangjinghuan@yahoo.com>
2022-07-05 16:05:26 +0300

    Decorator - two decorators on one class, the facotries run in order applied, but their corresponding decorator run bottom up order

--- dist/app.js
+++ dist/app.js
@@ -5,8 +5,18 @@ var __decorate = (this && this.__decorate) || function (decorators, target, key,
     else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
     return c > 3 && r && Object.defineProperty(target, key, r), r;
 };
-function WithTemplate(template, hookId) {
+function Log(logString) {
+    console.log("from Log decorator factory");
     return function (constructor) {
+        console.log("from Log decorator ........");
+        console.log("logString = ", logString);
+        console.log("constructor = ", constructor);
+    };
+}
+function WithTemplate(template, hookId) {
+    console.log("from WithTemplate decorator factory");
+    return function (constructor) {
+        console.log("from WithTamplate decorator ........");
         const p = new constructor();
         const hookElement = document.getElementById(hookId);
         if (hookElement) {
@@ -22,6 +32,7 @@ let Person = class Person {
     }
 };
 Person = __decorate([
+    Log("LOGIN - PERSON"),
     WithTemplate("<h1>This is from WithTemplate </h1>", "apple")
 ], Person);
 //# sourceMappingURL=app.js.map
--- src/app.ts
+++ src/app.ts
@@ -1,14 +1,21 @@
-// // create a decorator factory
-// function Log(logString: string) {
-//   return function (constructor: Function) {
-//     // console.log("now is printing from decorator...");
-//     console.log("logString = ", logString);
-//     console.log("constructor = ", constructor);
-//   };
-// }
+// create a decorator factory
+function Log(logString: string) {
+  console.log("from Log decorator factory");
+  return function (constructor: Function) {
+    // console.log("now is printing from decorator...");
+    console.log("from Log decorator ........");
+
+    console.log("logString = ", logString);
+    console.log("constructor = ", constructor);
+  };
+}
 
 function WithTemplate(template: string, hookId: string) {
+  console.log("from WithTemplate decorator factory");
+
   return function (constructor: any) {
+    console.log("from WithTamplate decorator ........");
+
     const p = new constructor();
     const hookElement = document.getElementById(hookId);
     if (hookElement) {
@@ -18,8 +25,7 @@ function WithTemplate(template: string, hookId: string) {
   };
 }
 
-// @Log("LOGIN - PERSON")
-
+@Log("LOGIN - PERSON")
 @WithTemplate("<h1>This is from WithTemplate </h1>", "apple")
 class Person {
   name = "Rose";
