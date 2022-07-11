c1d07e30149b9c12bd4c743ecb018f7bc911c130
cmjiwa <wangjinghuan@yahoo.com>
2022-07-11 22:40:21 +0300

    Returing a class in a class decorator - 1

--- dist/app.js
+++ dist/app.js
@@ -5,9 +5,6 @@ var __decorate = (this && this.__decorate) || function (decorators, target, key,
     else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
     return c > 3 && r && Object.defineProperty(target, key, r), r;
 };
-var __param = (this && this.__param) || function (paramIndex, decorator) {
-    return function (target, key) { decorator(target, key, paramIndex); }
-};
 function Log(logString) {
     console.log("from Log decorator factory");
     return function (constructor) {
@@ -18,14 +15,18 @@ function Log(logString) {
 }
 function WithTemplate(template, hookId) {
     console.log("from WithTemplate decorator factory");
-    return function (constructor) {
-        console.log("from WithTamplate decorator ........");
-        const p = new constructor();
-        const hookElement = document.getElementById(hookId);
-        if (hookElement) {
-            hookElement.innerHTML = template;
-            hookElement.querySelector("h1").textContent = p.name;
-        }
+    return function (originalconstructor) {
+        return class extends originalconstructor {
+            constructor(..._) {
+                super();
+                console.log("from WithTamplate decorator ........");
+                const hookElement = document.getElementById(hookId);
+                if (hookElement) {
+                    hookElement.innerHTML = template;
+                    hookElement.querySelector("h1").textContent = this.name;
+                }
+            }
+        };
     };
 }
 let Person = class Person {
@@ -38,57 +39,6 @@ Person = __decorate([
     Log("LOGIN - PERSON"),
     WithTemplate("<h1>This is from WithTemplate </h1>", "apple")
 ], Person);
-function Logger(target, propertyName) {
-    console.log("Property decorator called . . .");
-    console.log("target = ", target);
-    console.log("type of target = ", typeof target);
-    console.log(" propertyName = ", propertyName);
-}
-function Logger2(target, name, descriptor) {
-    console.log("Accessor decorator is called . . . ");
-    console.log("target from accessor decorator = ", target);
-    console.log("type of accessor decorator target = ", typeof target);
-    console.log("name from accessor decorator = ", name);
-    console.log("descriptor from accessor decorator = ", descriptor);
-    console.log("typeof descriptor from accessor decorator = ", typeof descriptor);
-}
-function Logger3(target, name, descriptor) {
-    console.log("Method decorator is called . . . ");
-    console.log("target from method decorator = ", target);
-    console.log("type of method decorator target = ", typeof target);
-    console.log("name from method decorator = ", name);
-    console.log("descriptor from method decorator = ", descriptor);
-}
-function Logger4(target, name, position) {
-    console.log("Parameter decorator is called . . . ");
-    console.log("target from parameter decorator = ", target);
-    console.log("type of parameter decorator target = ", typeof target);
-    console.log("name from parameter decorator = ", name);
-    console.log("descriptor from parameter decorator = ", position);
-}
-class Product {
-    constructor(p) {
-        this._price = p;
-        this.title = "bb";
-    }
-    set price(v) {
-        if (v > 0) {
-            this._price = v;
-        }
-        throw new Error("");
-    }
-    getPriceWithTax(tax) {
-        return this._price * (1 + tax);
-    }
-}
-__decorate([
-    Logger
-], Product.prototype, "title", void 0);
-__decorate([
-    Logger2
-], Product.prototype, "price", null);
-__decorate([
-    Logger3,
-    __param(0, Logger4)
-], Product.prototype, "getPriceWithTax", null);
+const aa = new Person();
+console.log("aa = ", aa);
 //# sourceMappingURL=app.js.map
--- src/app.ts
+++ src/app.ts
@@ -12,16 +12,19 @@ function Log(logString: string) {
 
 function WithTemplate(template: string, hookId: string) {
   console.log("from WithTemplate decorator factory");
+  return function (originalconstructor: any) {
+    return class extends originalconstructor {
+      constructor() {
+        super();
+        console.log("from WithTamplate decorator ........");
 
-  return function (constructor: any) {
-    console.log("from WithTamplate decorator ........");
-
-    const p = new constructor();
-    const hookElement = document.getElementById(hookId);
-    if (hookElement) {
-      hookElement.innerHTML = template;
-      hookElement.querySelector("h1")!.textContent = p.name;
-    }
+        const hookElement = document.getElementById(hookId);
+        if (hookElement) {
+          hookElement.innerHTML = template;
+          hookElement.querySelector("h1")!.textContent = this.name;
+        }
+      }
+    };
   };
 }
 
@@ -35,73 +38,72 @@ class Person {
   }
 }
 
-//const aa = new Person();
+const aa = new Person();
+console.log("aa = ", aa);
 
-//console.log("aa = ", aa);
+// define a property decorator
+// function Logger(target: any, propertyName: string | Symbol) {
+//   console.log("Property decorator called . . .");
+//   console.log("target = ", target);
+//   console.log("type of target = ", typeof target);
+//   console.log(" propertyName = ", propertyName);
+// }
 
-//define a property decorator
-function Logger(target: any, propertyName: string | Symbol) {
-  console.log("Property decorator called . . .");
-  console.log("target = ", target);
-  console.log("type of target = ", typeof target);
-  console.log(" propertyName = ", propertyName);
-}
+// //define an accessor decorator
+// function Logger2(target: any, name: string, descriptor: PropertyDescriptor) {
+//   console.log("Accessor decorator is called . . . ");
+//   console.log("target from accessor decorator = ", target);
+//   console.log("type of accessor decorator target = ", typeof target);
+//   console.log("name from accessor decorator = ", name);
+//   console.log("descriptor from accessor decorator = ", descriptor);
+//   console.log(
+//     "typeof descriptor from accessor decorator = ",
+//     typeof descriptor
+//   );
+// }
 
-//define an accessor decorator
-function Logger2(target: any, name: string, descriptor: PropertyDescriptor) {
-  console.log("Accessor decorator is called . . . ");
-  console.log("target from accessor decorator = ", target);
-  console.log("type of accessor decorator target = ", typeof target);
-  console.log("name from accessor decorator = ", name);
-  console.log("descriptor from accessor decorator = ", descriptor);
-  console.log(
-    "typeof descriptor from accessor decorator = ",
-    typeof descriptor
-  );
-}
+// //difine a method accessor
+// function Logger3(
+//   target: any,
+//   name: string | symbol,
+//   descriptor: PropertyDescriptor
+// ) {
+//   console.log("Method decorator is called . . . ");
+//   console.log("target from method decorator = ", target);
+//   console.log("type of method decorator target = ", typeof target);
+//   console.log("name from method decorator = ", name);
+//   console.log("descriptor from method decorator = ", descriptor);
+// }
 
-//difine a method accessor
-function Logger3(
-  target: any,
-  name: string | symbol,
-  descriptor: PropertyDescriptor
-) {
-  console.log("Method decorator is called . . . ");
-  console.log("target from method decorator = ", target);
-  console.log("type of method decorator target = ", typeof target);
-  console.log("name from method decorator = ", name);
-  console.log("descriptor from method decorator = ", descriptor);
-}
+// //define a parameter decorator
+// function Logger4(target: any, name: string | symbol, position: number) {
+//   console.log("Parameter decorator is called . . . ");
+//   console.log("target from parameter decorator = ", target);
+//   console.log("type of parameter decorator target = ", typeof target);
+//   console.log("name from parameter decorator = ", name);
+//   console.log("descriptor from parameter decorator = ", position);
+// }
 
-//define a parameter decorator
-function Logger4(target: any, name: string | symbol, position: number) {
-  console.log("Parameter decorator is called . . . ");
-  console.log("target from parameter decorator = ", target);
-  console.log("type of parameter decorator target = ", typeof target);
-  console.log("name from parameter decorator = ", name);
-  console.log("descriptor from parameter decorator = ", position);
-}
+// class Product {
+//   @Logger
+//   title: string;
+//   private _price: number;
 
-class Product {
-  @Logger
-  title: string;
-  private _price: number;
+//   constructor(p: number) {
+//     this._price = p;
+//     this.title = "bb";
+//   }
 
-  constructor(p: number) {
-    this._price = p;
-    this.title = "bb";
-  }
+//   @Logger2
+//   set price(v: number) {
+//     if (v > 0) {
+//       this._price = v;
+//     }
+//     throw new Error("");
+//   }
 
-  @Logger2
-  set price(v: number) {
-    if (v > 0) {
-      this._price = v;
-    }
-    throw new Error("");
-  }
-
-  @Logger3
-  getPriceWithTax(@Logger4 tax: number) {
-    return this._price * (1 + tax);
-  }
-}
+//   @Logger3
+//   getPriceWithTax(@Logger4 tax: number) {
+//     return this._price * (1 + tax);
+//   }
+// }
